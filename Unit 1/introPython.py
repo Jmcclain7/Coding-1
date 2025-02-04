708d482f5b2366a5fb07481cb82cfe4e9dc73d48
Jmcclain7 <jmcclain865@boyslatin.net>
2025-02-04 19:23:59 +0000

    Reviewed data types

--- Unit 1/introPython.py
+++ Unit 1/introPython.py
@@ -1,2 +1,26 @@
-message = 'hello from phython
-'words
+# message = 'hello from phython
+
+#The hastag symbol is a way to make
+# anything invisible to a computer in phython.
+
+# text behing hastag is great for
+# writing notes to yourself or to others
+# people
+
+# Datatypes
+#
+# "STRING DATA TYPE"
+# A string is anything that is
+# wrapped in quotes
+
+# "we create strings by first clicking the
+# quote button and then typing text within the quotes
+
+#"strings contain virtually any key on your keyboard"
+
+#"Strings are used mostly for text based data like messages, comments, etc."
+
+  #Integer Data Type
+# an integr=er is just another way of saying 
+# number
+# we don
