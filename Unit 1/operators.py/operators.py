3e2ed18699960496f3139b266f0218d8d20f7436
Jmcclain7 <jmcclain865@boyslatin.net>
2025-02-11 19:31:09 +0000

    operators lesson

---
+++ Unit 1/operators.py/operators.py
@@ -0,0 +1,39 @@
+# Data munipulation
+
+# In phython we have operators, that 
+# allow us to change data.
+
+# Assignment operators
+# assignment opperators use some combination of the equal
+# to give a variable a value.
+
+name = 'James' # the equal sign is the opertor
+willItsnow = True
+numberOfSandwiches = 10
+bankAccount = 10000
+
+# Comparison operators
+# comparisons operators allow us to evaluate data 
+# based on several factors
+
+10 < 20 # is 10 less than 20
+
+20.34 >= 0 #is 20.34 greater than or equal to 0
+
+100 = 100 # is 100 ethe SAME as 100
+
+100 != 10 # is 100 NOT theSAME AS 10
+
+# Arithmatic operators
+# the arithmatic operators allow us to perform 
+# math with- specifically with the integer and float
+# data types
+
+10 + 10 # addition
+12 - 2 # subtraction
+10 * 3 # multiplication
+2 / 1 # division
+
+# Logical operators
+# we use logical operators to evaluate if data is true
+# or false
