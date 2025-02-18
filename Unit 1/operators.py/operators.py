6907990a7d1005061a8b029125aeb34480e81e40
Jmcclain7 <jmcclain865@boyslatin.net>
2025-02-18 19:25:03 +0000

    Completed notes on operators

--- Unit 1/operators.py/operators.py
+++ Unit 1/operators.py/operators.py
@@ -12,6 +12,8 @@ willItsnow = True
 numberOfSandwiches = 10
 bankAccount = 10000
 
+
+laptopDell= 1000
 # Comparison operators
 # comparisons operators allow us to evaluate data 
 # based on several factors
@@ -34,6 +36,25 @@ bankAccount = 10000
 10 * 3 # multiplication
 2 / 1 # division
 
+
 # Logical operators
 # we use logical operators to evaluate if data is true
 # or false
+password = '123'
+securityQuestion = 'Frances'
+
+mathAnswer = 4
+# and - used to compare to conditons that are both true
+
+password == '123' and 4 == mathAnswer
+#or - used to compare conditions, But onlt one needs to
+#to be true in order for the output to be true
+
+password == '123' or 2+3 ==mathAnswer
+
+#not - used to compare conditions, BUT will
+# output the opposite result - meaning if two things are true
+# we are going to get false
+
+# whatever the real result is, will become the opposite
+not(password == '123' and 2=3 == mathAnswer)
