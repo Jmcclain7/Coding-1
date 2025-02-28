9e27be4081937a2fec5a1fe0e1c1c790c246aaf7
Jmcclain7 <jmcclain865@boyslatin.net>
2025-02-28 19:26:54 +0000

    Async work Feb 28th

---
+++ Unit 1/operators.py/functions.py
@@ -0,0 +1,28 @@
+def compare():
+    x = 10
+    y = 20
+    print(x<y)
+
+compare()
+
+def text():
+    title = 'engineer'
+    years = 10
+    print(title)
+    print(years)
+
+def greeting():
+    print("welcome to python functions.")
+
+greeting()
+
+def computer():
+    x = 10
+    y = 20
+    xy = 30
+    print(x + y + xy)
+
+computer()
+
+
+
