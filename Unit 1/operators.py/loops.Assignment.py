34aa3c205830d27d29b55474fc7658326d1c0de5
Jmcclain7 <jmcclain865@boyslatin.net>
2025-04-24 18:24:33 +0000

    Review Work

--- Unit 1/operators.py/loops.Assignment.py
+++ Unit 1/operators.py/loops.Assignment.py
@@ -5,4 +5,28 @@ def wordGuess():
     else:
         print('No')
 
-wordGuess(Conquest)
+wordGuess()
+
+
+
+# For Loops - are a type of loop that runs
+# specific conditions on a list of data.
+
+# List - a collection data type for storing 
+# different types of data.
+
+bobby_Coding = ['Bobby', 16, True, 96.6]
+david_Coding = ['Bobby', 15, False, 85.9]
+
+blPowerSchool_Class = [bobby_Coding, david_Coding]
+
+class BLHS:
+    grade_9:[student]
+    grade_10:[]
+    grade_11:[]
+
+
+class student:
+    name:''
+    grade:
+    classes:
---
+++ Unit 1/operators.py/review.py
@@ -0,0 +1,34 @@
+def depositMoney(money):
+    balance = 0 # step 1: get current balance 
+    newBalance= money + balance # step2 : add new money to balance 
+    print('your new balance is : ' + str(balance)) #step3: print out balance
+
+    depositMoney(100) 
+
+    def withdraw():
+
+    # conditional statements
+
+    apples = 10
+    if apples > 3:
+        print("share your aples")
+    else:
+        print('you dont have enough to share')
+
+    # rollercoaster
+    # banking
+
+    youtubePayout_1000 = 100
+    youtubePayout_100 = 100
+
+    payRate =0.18
+    
+    #mounthly
+    views= 1001
+
+    if views > youtubePayout_100:
+        print("congrats! heres your 500 dollars")
+    elif views > youtubePayout_100:
+        print("congrats! heres your 100 dolllars")
+    else:
+        print("sorry, your channel isnt there yet. ")
